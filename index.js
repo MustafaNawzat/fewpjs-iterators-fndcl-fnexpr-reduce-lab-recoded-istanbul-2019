1ad696791eae63214e71cd2c3e0050e65e909b47
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-04 09:44:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,3 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = batteryBatches.reduce(function(total){
   return total;
 })
-
-
-
-
-    var numbers = [1,2,3]; 
-    var sum = numbers.reduce(function(total, current){
-        return total + current;
-    });
